7ffe6bc37e07fc8780b3a8dc0d362caccdacceb3
Defeat3d <koen.deubel@hva.nl>
2020-12-19 14:50:57 +0100

    Compress data, persist every 15 minutes, global markov

--- src/main/kotlin/com/defiled/discov/Discov.kt
+++ src/main/kotlin/com/defiled/discov/Discov.kt
@@ -7,7 +7,11 @@ import discord4j.common.util.Snowflake
 import discord4j.core.DiscordClient
 import discord4j.core.GatewayDiscordClient
 import discord4j.core.event.domain.message.MessageCreateEvent
+import java.io.ByteArrayOutputStream
 import java.io.File
+import java.util.concurrent.TimeUnit
+import java.util.zip.GZIPInputStream
+import java.util.zip.GZIPOutputStream
 
 internal object Discov {
 
@@ -20,8 +24,15 @@ internal object Discov {
 
     private lateinit var markovs: MutableMap<Snowflake, Markov>
 
+    private var lastSave = 0L
+
     fun initialize(gateway: GatewayDiscordClient) {
-        markovs = if (storageFile.exists()) gson.fromJson(storageFile.readText(), TypeToken.getParameterized(MutableMap::class.java, Snowflake::class.java, Markov::class.java).type) else mutableMapOf()
+        markovs = if (storageFile.exists()) {
+            val json = ungzip(storageFile.readBytes())
+            gson.fromJson(json, TypeToken.getParameterized(MutableMap::class.java, Snowflake::class.java, Markov::class.java).type)
+        } else {
+            mutableMapOf()
+        }
 
         gateway.on(MessageCreateEvent::class.java).subscribe { event ->
             val msg = event.message
@@ -30,19 +41,28 @@ internal object Discov {
             val author = msg.authorAsMember.block() ?: return@subscribe
 
             if (!msg.content.startsWith("!markov")) {
-                val authorMarkov = markovs.getOrPut(author.id) { Markov() }
-                authorMarkov.addPhrase(formatContent(msg.content))
+                if (author.id != gateway.selfId) {
+                    val authorMarkov = markovs.getOrPut(author.id) { Markov() }
+                    val globalMarkov = markovs.getOrPut(gateway.selfId) { Markov() }
+                    val formatted = formatContent(msg.content)
+                    authorMarkov.addPhrase(formatted)
+                    globalMarkov.addPhrase(formatted)
+
+                    if (System.currentTimeMillis() - lastSave > TimeUnit.MINUTES.toMillis(15)) {
+                        val json = gson.toJson(markovs)
+                        storageFile.writeBytes(gzip(json))
+                        lastSave = System.currentTimeMillis()
+                    }
+                }
             } else if (author.id != gateway.selfId) {
                 if (msg.content.startsWith("!markov purge")) {
                     markovs.remove(author.id)
                 }
-                val mention = msg.userMentions.blockFirst() ?: return@subscribe
-                val mentionedMarkov = markovs[mention.id] ?: return@subscribe
-                val generated = mentionedMarkov.generate() ?: return@subscribe
+                val userId = msg.userMentions.blockFirst()?.id ?: gateway.selfId ?: return@subscribe
+                val markov = markovs[userId] ?: return@subscribe
+                val generated = markov.generate() ?: return@subscribe
                 channel.createMessage(generated).block()
             }
-
-            storageFile.writeText(gson.toJson(markovs))
         }
 
         gateway.onDisconnect().block()
@@ -52,6 +72,14 @@ internal object Discov {
         val capitalized = content.capitalize()
         return if (capitalized.lastOrNull() in setOf('.', '?', '!')) capitalized else "$capitalized."
     }
+
+    private fun gzip(content: String): ByteArray {
+        val bos = ByteArrayOutputStream()
+        GZIPOutputStream(bos).bufferedWriter().use { it.write(content) }
+        return bos.toByteArray()
+    }
+
+    private fun ungzip(content: ByteArray) = GZIPInputStream(content.inputStream()).bufferedReader().use { it.readText() }
 
 }
 
